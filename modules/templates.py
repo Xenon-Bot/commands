56aff415d06818b2921ef1298150b0fdd661cbbf
Merlintor <merlinfuchs2001@gmail.com>
2021-03-13 12:00:15 +0100

    fix template commands

--- modules/templates.py
+++ modules/templates.py
@@ -3,8 +3,12 @@ from dbots.cmd import *
 import re
 import asyncio
 from datetime import timedelta, datetime
+from grpclib.exceptions import GRPCError
+from dbots.protos import backups_pb2
+import grpclib
 
 from .audit_logs import AuditLogType
+from .backups import warning_list, convert_v1_to_v2, channel_tree
 
 
 class TemplatesModule(Module):
@@ -31,15 +35,37 @@ class TemplatesModule(Module):
                 "approved": True,
                 "data": {
                     "id": data["source_guild_id"],
+                    "name": data["name"],
+                    "afk_channel_id": str(data["afk_channel_id"]) if data.get("afk_channel_id") else None,
+                    "system_channel_id": str(data["system_channel_id"]) if data.get("system_channel_id") else None,
+                    "system_channel_flags": data.get("system_channel_flags"),
+                    "verification_level": data.get("verification_level"),
+                    "afk_timeout": data.get("verification_level"),
+                    "default_message_notifications": data.get("default_message_notifications"),
+                    "explicit_content_filter": data.get("explicit_content_filter"),
                     "roles": [
                         {
                             "position": pos,
+                            "id": str(r.pop("id")),
                             **r
                         }
                         for pos, r in enumerate(guild.pop("roles", []))
                     ],
-                    "mfa_level": 0,
-                    **guild
+                    "channels": [
+                        {
+                            "id": str(c.pop("id")),
+                            "parent_id": str(c.pop("parent_id")) if c.get("parent_id") else None,
+                            "permission_overwrites": [
+                                {
+                                    "id": str(ov.pop("id")),
+                                    **ov
+                                }
+                                for ov in c.pop("permission_overwrites", [])
+                            ],
+                            **c
+                        }
+                        for c in guild.pop("channels", [])
+                    ],
                 }
             }
         except rest.HTTPNotFound:
@@ -57,7 +83,7 @@ class TemplatesModule(Module):
         Create a new public template
         """
         await ctx.respond(
-            "Please use https://templates.xenon.bot to add new templates, "
+            "Please use [templates.xenon.bot](https://templates.xenon.bot) to add new templates, "
             "you can find help on the [wiki](https://wiki.xenon.bot/en/templates#creating-a-template) "
             "for how to create new templates.",
             ephemeral=True
@@ -73,14 +99,15 @@ class TemplatesModule(Module):
     @checks.has_permissions_level(destructive=True)
     @checks.bot_has_permissions("administrator")
     @checks.not_in_maintenance
-    @checks.cooldown(1, 60, bucket=checks.CooldownType.GUILD, manual=True)
-    async def load(self, ctx, identifier, options=""):
+    @checks.cooldown(1, 5 * 60, bucket=checks.CooldownType.GUILD, manual=True)
+    async def load(self, ctx, name_or_id, options=""):
         """
         Load one of the public templates
 
         You can find more help on the [wiki](https://wiki.xenon.bot/templates#loading-a-template).
         """
-        template = await self._get_template(identifier)
+        template = await self._get_template(name_or_id)
+        data = convert_v1_to_v2(template["data"])
         if template is None:
             await ctx.respond_with_source(**create_message(
                 f"Can't find a template with the name, id or url `{template}`.\n"
@@ -89,136 +116,36 @@ class TemplatesModule(Module):
             ))
             return
 
-        if await ctx.bot.redis.exists(f"cmd:loaders:{ctx.guild_id}"):
-            await ctx.respond_with_source(**create_message(
-                "There is **already** a backup or template loader **running**. "
-                "You can't start more than one at the same time.\n"
-                "You have to **wait until it's done** or use `/template cancel` to cancel the loader..",
-                f=Format.ERROR
-            ))
-            return
-
-        await ctx.ack_with_source()
-        await asyncio.sleep(0.2)
-
-        # Fill options object
-        parsed_options = LoaderOptions("delete-channels delete-roles channels roles settings")
-        parsed_options.update(options)
-        parsed_options.update("!messages !members !bans")
+        allowed = ("delete_roles", "delete_channels", "roles", "channels", "settings")
+        parsed_options = {"delete_roles", "delete_channels", "roles", "channels", "settings"}
+        for option in options.replace("-", "_").split(" "):
+            if option == "!*":
+                parsed_options.clear()
+            elif option == "*":
+                parsed_options = set(allowed)
+            elif option.startswith("!"):
+                try:
+                    parsed_options.remove(option[1:])
+                except KeyError:
+                    pass
+            elif option in allowed:
+                parsed_options.add(option)
 
         # Require a confirmation by the user
-        status_msg = Message(await ctx.respond(**create_message(
-            warning_text(parsed_options),
+        await ctx.respond(**create_message(
+            "**Hey, be careful!** The following actions will be taken on this server and **can not be undone**:\n\n"
+            f"{warning_list(parsed_options)}\n\n"
+            f"Type `/confirm` to confirm this action and continue.",
             f=Format.WARNING
-        )))
+        ))
 
-        confirmed = await reaction_confirmation(ctx.bot, status_msg, ctx.author.id)
-        if not confirmed:
-            await ctx.edit_response(**create_message(
-                "You didn't confirm to load the template so the **loading process was cancelled**.\n"
-                f"If this was not intended use `/template load template: {identifier}` to try again.",
-                f=Format.INFO
-            ), message_id=status_msg.id)
+        try:
+            await self.bot.wait_for_confirmation(ctx, timeout=60)
+        except asyncio.TimeoutError:
+            await ctx.delete_response()
             return
 
         await ctx.count_cooldown()
-
-        # TODO: Publish loaders:start event
-
-        loader = GuildLoader(
-            self.bot,
-            ctx.guild_id,
-            template["data"],
-            options=parsed_options,
-            ignore=[ctx.channel_id],
-            reason="Template loaded by " + str(ctx.author)
-        )
-
-        # Inject previous id translators if available
-        translator = await ctx.bot.db.id_translators.find_one({
-            "target_id": ctx.guild_id,
-            "source_id": loader.data["id"]
-        })
-        if translator is not None:
-            loader.ids.update(translator["ids"])
-
-        try:
-            async for option in run_loader(loader):
-                # TODO: Publish loaders:status event
-                try:
-                    await ctx.edit_response(**create_message(
-                        status_text(parsed_options, option),
-                        title="Loading Status",
-                        f=Format.PLEASE_WAIT
-                    ), message_id=status_msg.id)
-                except rest.HTTPException:
-                    pass
-
-        except asyncio.CancelledError:
-            try:
-                await ctx.edit_response(**create_message(
-                    "The **loading process was cancelled**.",
-                    f=Format.ERROR
-                ), message_id=status_msg.id)
-            except rest.HTTPException:
-                pass
-
-        except RoleRateLimit as e:
-            remaining = timedelta(hours=48)
-            if e.ratelimit is not None:
-                remaining = timedelta(seconds=e.ratelimit.delta)
-
-            try:
-                await ctx.edit_response(**create_message(
-                    "Seems like you **hit** the `250 per 48 hours` **role creation limit** of discord.\n"
-                    "Xenon is only allowed to create 250 roles in a time frame of 48 hours.\n\n"
-                    f"**You have to wait** `{timedelta_to_string(remaining, precision='m')}` "
-                    f"before loading another backup or template containing roles.\n\n"
-                    f"**This is a discord limitation and there is no way around it.**",
-                    f=Format.ERROR
-                ), message_id=status_msg.id)
-            except rest.HTTPException:
-                pass
-
-        else:
-            try:
-                await ctx.edit_response(**create_message(
-                    f"Successfully **loaded the template**.",
-                    f=Format.SUCCESS
-                ), message_id=status_msg.id)
-            except rest.HTTPException:
-                pass
-
-            if parsed_options.get("delete_channels"):
-                await asyncio.sleep(5)
-                try:
-                    await ctx.bot.http.delete_channel(ctx.channel_id)
-                except rest.HTTPException:
-                    pass
-
-        # Save ids for later use and recovery
-        await ctx.bot.db.id_translators.update_one(
-            {
-                "target_id": ctx.guild_id,
-                "source_id": loader.data["id"],
-            },
-            {
-                "$set": {
-                    "target_id": ctx.guild_id,
-                    "source_id": loader.data["id"],
-                    **{
-                        f"ids.{s}": t
-                        for s, t in loader.ids.items()
-                    }
-                },
-                "$addToSet": {
-                    "loaders": ctx.author.id
-                }
-            },
-            upsert=True
-        )
-
-        # TODO: Publish loaders:done event
 
         # Create audit log entry
         await self.bot.db.audit_logs.insert_one({
@@ -229,15 +156,92 @@ class TemplatesModule(Module):
             "extra": {}
         })
 
+        ids = {}
+        translator = await ctx.bot.db.id_translators.find_one({
+            "target_id": ctx.guild_id,
+            "source_id": data.id
+        })
+        if translator is not None:
+            ids = translator["ids"]
+
+        await ctx.edit_response(**create_message(
+            "**The template is now loading**. Please be patient, this can take a while!\n\n"
+            "Use `/template status` to get the current status and `/template cancel` to cancel the process.\n\n"
+            "*This message will not be updated.*",
+            f=Format.INFO
+        ))
+        await asyncio.sleep(5)
+
+        try:
+            replies = await self.bot.rpc.backups.Load(backups_pb2.LoadRequest(
+                guild_id=ctx.guild_id,
+                options=list(parsed_options),
+                message_count=0,
+                data=data,
+                reason="Template loaded by " + str(ctx.author),
+                ids=ids
+            ))
+        except GRPCError as e:
+            if e.status == grpclib.Status.ALREADY_EXISTS:
+                await ctx.edit_response(**create_message(
+                    f"There is **already a loading process running** on this server.\n"
+                    f"Please wait for it to finish or use `/template cancel` to stop it.",
+                    f=Format.ERROR
+                ))
+                return
+            else:
+                raise
+
+        try:
+            await ctx.edit_response(**create_message(
+                f"Successfully **loaded the template**.",
+                f=Format.SUCCESS
+            ))
+        except rest.HTTPNotFound:
+            pass
+
+        # Save ids for later use and recovery
+        await ctx.bot.db.id_translators.update_one(
+            {
+                "target_id": ctx.guild_id,
+                "source_id": data.id,
+            },
+            {
+                "$set": {
+                    "target_id": ctx.guild_id,
+                    "source_id": data.id,
+                    **{
+                        f"ids.{s}": t
+                        for s, t in replies[-1].ids.items()
+                    }
+                },
+                "$addToSet": {
+                    "loaders": ctx.author.id
+                }
+            },
+            upsert=True
+        )
+
     @template.sub_command()
     @checks.has_permissions_level(destructive=True)
     async def cancel(self, ctx):
         """
         Cancel the currently running loading process on this server
         """
-        await ctx.bot.redis.delete(f"cmd:loaders:{ctx.guild_id}")
-        await ctx.respond_with_source(**create_message(
-            "Successfully **cancelled the currently running loader** on this server.",
+        try:
+            await self.bot.rpc.backups.CancelLoad(backups_pb2.CancelLoadRequest(guild_id=ctx.guild_id))
+        except GRPCError as e:
+            if e.status == grpclib.Status.NOT_FOUND:
+                await ctx.respond(**create_message(
+                    "There is **no loading process running** on this server.",
+                    f=Format.ERROR
+                ))
+                return
+            else:
+                raise
+
+        await ctx.respond(**create_message(
+            "Successfully **cancelled the currently running loading process** on this server.",
             f=Format.SUCCESS
         ))
 
@@ -247,17 +251,20 @@ class TemplatesModule(Module):
         """
         Get the status of the currently running loading process
         """
-        status = await ctx.bot.redis.get(f"cmd:loaders:{ctx.guild_id}")
-        if status is None:
-            await ctx.respond_with_source(**create_message(
-                "There is currently no loader running on this server",
-                f=Format.ERROR
-            ))
-            return
+        try:
+            reply = await self.bot.rpc.backups.LoadStatus(backups_pb2.LoadStatusRequest(guild_id=ctx.guild_id))
+        except GRPCError as e:
+            if e.status == grpclib.Status.NOT_FOUND:
+                await ctx.respond(**create_message(
+                    "There is **no loading process running** on this server.",
+                    f=Format.ERROR
+                ))
+                return
+            else:
+                raise
 
-        status_text = option_text[status.decode()]
-        await ctx.respond_with_source(**create_message(
-            f"{status_text} ...\n\n"
+        await ctx.respond(**create_message(
+            f"{reply.status} ...\n\n"
             f"*Please be patient, this could take a while.*",
             title="Loader Status",
             f=Format.INFO
@@ -281,11 +288,12 @@ class TemplatesModule(Module):
             )
         )
     )
-    async def info(self, ctx, identifier):
+    async def info(self, ctx, name_or_id):
         """
         Get information about a public template
         """
-        template = await self._get_template(identifier)
+        template = await self._get_template(name_or_id)
+        data = convert_v1_to_v2(template["data"])
         if template is None:
             await ctx.respond_with_source(**create_message(
                 f"Can't find a template with the name, id or url `{template}`.\n"
@@ -294,31 +302,33 @@ class TemplatesModule(Module):
             ))
             return
 
-        data = template["data"]
-
-        channel_list = channel_tree([Channel(c) for c in data["channels"]])
+        channel_list = channel_tree(data.channels)
         if len(channel_list) > 1024:
             channel_list = channel_list[:1000] + "\n...\n```"
 
-        roles = [Role(r) for r in data["roles"]]
         role_list = "```{}```".format("\n".join(
-            [r.name for r in sorted(roles, key=lambda r: r.position, reverse=True)]
+            [r.name for r in sorted(data.roles, key=lambda r: r.position, reverse=True)]
         ))
         if len(role_list) > 1024:
             role_list = role_list[:1000] + "\n...\n```"
 
-        await ctx.respond_with_source(embeds=[{
-            "title": f"Template Info - *{data['name']}*",
+        await ctx.respond(embeds=[{
+            "title": f"Template Info - *{data.name}*",
             "color": Format.INFO.color,
             "fields": [
                 {
-                    "name": "Creator",
-                    "value": f"<@{template['creator_id']}>",
-                    "inline": False
+                    "name": "Used By",
+                    "value": f"{template['usage_count']} people",
+                    "inline": True
                 },
                 {
-                    "name": "Uses",
-                    "value": str(template["usage_count"]),
+                    "name": "Created By",
+                    "value": f"<@{template['creator_id']}>",
+                    "inline": True
+                },
+                {
+                    "name": "Description",
+                    "value": f"{template['description'][:1000]}",
                     "inline": False
                 },
                 {
