c7dafe9c27734a7ce4b7c3adf615466a3f723acf
Merlintor <merlinfuchs2001@gmail.com>
2021-04-01 15:55:46 +0200

    ignore errors in the error handler itself

--- bot.py
+++ bot.py
@@ -68,12 +68,15 @@ class Xenon(InteractionBot):
                 "author": ctx.author.id,
                 "traceback": tb
             }))
-            await ctx.respond(**create_message(
-                "An unexpected error occurred. Please report this on the "
-                "[Support Server](https://xenon.bot/discord).\n\n"
-                f"**Error Code**: `{error_id.upper()}`",
-                f=Format.ERROR
-            ))
+            try:
+                await ctx.respond(**create_message(
+                    "An unexpected error occurred. Please report this on the "
+                    "[Support Server](https://xenon.bot/discord).\n\n"
+                    f"**Error Code**: `{error_id.upper()}`",
+                    f=Format.ERROR
+                ))
+            except rest.HTTPException:
+                pass
 
     async def execute_command(self, command, payload, remaining_options):
         await self.redis.hincrby("cmd:commands", command.full_name, 1)
