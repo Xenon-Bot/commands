9872ab6f07a97a539c78e892cfdf1283e5d0c49b
Merlintor <merlinfuchs2001@gmail.com>
2021-07-27 15:19:51 +0200

    Add backup selection to unknown backup error

--- modules/backups.py
+++ modules/backups.py
@@ -15,6 +15,7 @@ import base64
 import hashlib
 import binascii
 import json
+from copy import deepcopy
 
 from .audit_logs import AuditLogType
 from . import encryption
@@ -242,6 +243,45 @@ class BackupsModule(Module):
             unique=True
         )
 
+    async def _unknown_backup_message(self, user_id, backup_id):
+        data = deepcopy(create_message(
+            f"You have **no backup** with the id `{backup_id}`.\n\n"
+            f"*Keep in mind that you can only access your own backups.*",
+            f=Format.ERROR
+        ))
+
+        select_options = []
+        backups = self.bot.db.backups.find(
+            {"creator": user_id},
+            sort=[("timestamp", pymongo.DESCENDING)],
+            projection=("_id", "data.name", "timestamp", "encrypted"),
+            limit=25
+        )
+        async for backup in backups:
+            if backup.get("encrypted"):
+                continue
+
+            _backup_id = backup["_id"].upper()
+            select_options.append(SelectMenuOption(
+                label=_backup_id,
+                description=f"{backup['data']['name']} ({datetime_to_string(backup['timestamp'])} UTC)"[:50],
+                value=_backup_id
+            ))
+
+        if len(select_options) != 0:
+            data.setdefault("components", []).insert(0, ActionRow(
+                SelectMenu(
+                    *select_options,
+                    placeholder="Select a backup",
+                    custom_id="backup_info_direct",
+                    min_values=1,
+                    max_values=1
+                ),
+            ))
+
+        data["ephemeral"] = True
+        return data
+
     @Module.command()
     async def backup(self, ctx):
         """
@@ -312,14 +352,11 @@ class BackupsModule(Module):
             "extra": {}
         })
 
-    async def _backup_load(self, ctx, backup_id, options):
+    async def _backup_load(self, ctx, backup_id, options, edit=False):
         exists = await self._backup_exists(ctx.author.id, backup_id)
         if not exists:
-            await ctx.respond(**create_message(
-                f"You have **no backup** with the id `{backup_id}`.\n\n"
-                f"*Keep in mind that you can only access your own backups.*",
-                f=Format.ERROR
-            ), ephemeral=True)
+            data = await self._unknown_backup_message(ctx.author.id, backup_id)
+            await ctx.respond(**data)
             return
 
         parsed_options = parse_options(
@@ -333,7 +370,10 @@ class BackupsModule(Module):
             "backup_id": backup_id,
             "options": list(parsed_options)
         }))
-        await ctx.respond(**create_warning_message(parsed_options, redis_key), ephemeral=True)
+        if edit:
+            await ctx.edit_response(**create_warning_message(parsed_options, redis_key))
+        else:
+            await ctx.respond(**create_warning_message(parsed_options, redis_key), ephemeral=True)
 
     @backup.sub_command(extends=dict(
         backup_id="The id of the previously created backup",
@@ -354,7 +394,7 @@ class BackupsModule(Module):
 
     @Module.component(name="backup_load_direct")
     async def load_direct(self, ctx, backup_id):
-        return await self._backup_load(ctx, backup_id, "")
+        return await self._backup_load(ctx, backup_id, "", edit=True)
 
     @Module.component(name="backup_load_options")
     async def load_options(self, ctx, redis_key):
@@ -662,11 +702,8 @@ class BackupsModule(Module):
     async def _backup_info(self, ctx, backup_id):
         data = await self._backup_info_message(ctx.author.id, backup_id, direct_load=ctx.guild_id is not None)
         if data is None:
-            await ctx.respond(**create_message(
-                f"You have **no backup** with the id `{backup_id}`.\n\n"
-                f"*Keep in mind that you can only access your own backups.*",
-                f=Format.ERROR
-            ), ephemeral=True)
+            data = await self._unknown_backup_message(ctx.author.id, backup_id)
+            await ctx.respond(**data)
             return
 
         await ctx.respond(**data)
@@ -745,7 +782,7 @@ class BackupsModule(Module):
 
             select_options.append(SelectMenuOption(
                 label=backup_id,
-                description=f"{backup['data']['name']} ({datetime_to_string(backup['timestamp'])} UTC)"[50:],
+                description=f"{backup['data']['name']} ({datetime_to_string(backup['timestamp'])} UTC)"[:50],
                 value=backup_id
             ))
 
@@ -828,10 +865,24 @@ class BackupsModule(Module):
             ), ephemeral=True)
 
         else:
-            await ctx.respond(**create_message(
-                f"You have **no backup** with the id `{backup_id}`.",
-                f=Format.ERROR
-            ), ephemeral=True)
+            data = await self._unknown_backup_message(ctx.author.id, backup_id)
+            await ctx.respond(**data)
+
+    @Module.component(name="backup_delete_direct")
+    async def delete_direct(self, ctx, backup_id):
+        if len(backup_id) > 20:
+            try:
+                key_bytes = encryption.id_to_key(backup_id)
+            except binascii.Error:
+                pass
+            else:
+                backup_id = base64.b64encode(hashlib.sha3_512(key_bytes).digest()).decode()
+
+        await self._delete_backup(ctx.author.id, backup_id)
+        await ctx.edit_response(**create_message(
+            "Successfully **deleted backup**.",
+            f=Format.SUCCESS
+        ), ephemeral=True)
 
     @backup.sub_command(
         extends=dict(
