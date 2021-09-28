0b90a8a633619d6c46f4aef1e6fd4117b676cefd
Merlintor <merlinfuchs2001@gmail.com>
2021-09-28 14:01:42 +0100

    add autocomplete to help command and add a faq command

--- modules/basics.py
+++ modules/basics.py
@@ -1,6 +1,33 @@
 from dbots.cmd import *
 import asyncio
 
+FAQ = {
+    "How do I invite Xenon to my server?":
+        "Please click [here](<https://xenon.bot/invite>) to invite Xenon to your server.",
+    "Is Xenon safe to use?":
+        "",
+    "I received an error, what do I do?":
+        "Please read the error carefully and try to understand it. "
+        "If you can't make sense of the error please join our [support server](https://xenon.bot/discord) "
+        "and ask in the support channel.",
+    "How do I load or create a backup?":
+        "You can create a backup of your server using the `/backup create` command. "
+        "You can find a list of your previously created backup using the `/backup list` command. "
+        "Use the `/backup load` command to load a backup on any server.",
+    "How do I load or create a template?":
+        "You can find a list of templates and instructions on how to load them at https://templates.xenon.bot . "
+        "If you want to create your own template please follow the instructions "
+        "[here](<https://wiki.xenon.bot/templates#creating-a-template>).",
+    "Where can I get Xenon Premium?":
+        "You can find more information about Xenon Premium [here](<https://wiki.xenon.bot/en/premium>)"
+        " and buy it [here](<https://www.patreon.com/merlinfuchs>).",
+    "I bought Xenon Premium but I'm not able to use it?!":
+        "Please join our [support server](<https://xenon.bot/discord>) and go to "
+        "<https://www.patreon.com/settings/apps> to connect your discord account to patreon. "
+        "You should see a channel called `#patrons-info` with instructions on how to activate premium. "
+        "If you still can't see the channel try to disconnect and reconnect your discord on patreon."
+}
+
 
 class BasicsModule(Module):
     @Module.command()
@@ -28,10 +55,65 @@ class BasicsModule(Module):
     async def leave_cancel(self, ctx):
         await ctx.update("Cool, I will stay! :)")
 
+    async def _faq_question_autocomplete(self, ctx, question):
+        matching = [
+            (q, q)
+            for q in FAQ
+            if question.strip() in q or q in question
+        ]
+
+        if len(matching) == 0:
+            matching.append(("As your question on our support server.", question))
+
+        return InteractionResponse.autocomplete(*matching[:25])
+
+    @Module.command(extends=dict(
+        question=dict(
+            description="Your question",
+            autocomplete=_faq_question_autocomplete
+        )
+    ))
+    async def faq(self, ctx, question):
+        """
+        You need help? Find the answer to your question here!
+        """
+        try:
+            answer = FAQ[question]
+        except KeyError:
+            answer = "Please join our [support server](<https://xenon.bot/discord>) and ask " \
+                     "your question in the support channel."
+
+        await ctx.respond(
+            f"**{question}**\n\n{answer}\n\n*This didn't answer your question? Join our [support server](<https://xenon.bot/discord>).*",
+            ephemeral=True
+        )
+
+    def _flattened_command_list(self):
+        for cmd in self.bot.commands:
+            if len(cmd.sub_commands) == 0:
+                yield cmd.name, cmd
+            else:
+                for sub_cmd in cmd.sub_commands:
+                    if isinstance(sub_cmd, SubCommand) or len(sub_cmd.sub_commands) == 0:
+                        yield f"{cmd.name} {sub_cmd.name}", sub_cmd
+                    else:
+                        for sub_sub_cmd in sub_cmd.sub_commands:
+                            yield f"{cmd.name} {sub_cmd.name} {sub_sub_cmd.name}", sub_sub_cmd
+
+    async def _help_command_autocomplete(self, ctx, command):
+        matched = [
+            (name, name)
+            for name, _ in self._flattened_command_list()
+            if command.strip() in name
+        ]
+
+        return InteractionResponse.autocomplete(*matched[:25])
+
     @Module.command(
         extends=dict(
             command=dict(
-                description="The full name of the command"
+                description="The full name of the command",
+                autocomplete=_help_command_autocomplete
             )
         )
     )
@@ -39,25 +121,13 @@ class BasicsModule(Module):
         """
         Get a list of commands or more information about a specific command
         """
-        def find_command():
-            parts = command.strip(" /").split(" ")
-            for cmd in ctx.bot.commands:
-                if parts[0] == cmd.name:
-                    if len(parts) == 1:
-                        return cmd
+        for name, cmd in self._flattened_command_list():
+            if name == command:
+                break
+        else:
+            cmd = None
 
-                    for sub_cmd in cmd.sub_commands:
-                        if parts[1] == sub_cmd.name:
-                            if len(parts) == 2 or not isinstance(sub_cmd, SubCommandGroup):
-                                return sub_cmd
-
-                            for sub_sub_cmd in sub_cmd.sub_commands:
-                                if parts[2] == sub_sub_cmd.name:
-                                    return sub_sub_cmd
-
-            return None
-
-        if command is None:
+        if cmd is None:
             await ctx.respond(
                 "**Xenon Help**\n\n"
                 "__Useful Commands__\n"
@@ -78,8 +148,7 @@ class BasicsModule(Module):
             )
             return
 
-        cmd = find_command()
-        if cmd is not None:
+        else:
             arg_list = "\n".join([f"**{option.name}**: *{option.description}*" for option in cmd.options])
             await ctx.respond(
                 f"**/{cmd.full_name}**\n\n"
@@ -88,12 +157,6 @@ class BasicsModule(Module):
                 f"{arg_list}",
                 ephemeral=True
             )
-
-        else:
-            await ctx.respond(**create_message(
-                f"Unknown command: `{command}`",
-                f=Format.ERROR
-            ), ephemeral=True)
 
     @Module.command()
     async def ping(self, ctx):
