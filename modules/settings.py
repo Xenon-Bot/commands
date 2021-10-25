429f338eb27610f8ec432e1d0cd9832c09bf4e0c
Merlintor <merlinfuchs2001@gmail.com>
2021-10-25 22:10:51 +0200

    add opt out and opt in command for privacy

--- modules/settings.py
+++ modules/settings.py
@@ -103,3 +103,63 @@ class SettingsModule(Module):
             f"*Use `/help settings permissions` to get more info.*",
             f=Format.SUCCESS
         ), ephemeral=True)
+
+    @Module.command()
+    async def opt(self, ctx):
+        """
+        Opt in and out of end-user-data collection
+        """
+
+    @opt.sub_command(name="out")
+    async def opt_out(self, ctx):
+        """
+        Opt out of end-user-data collection for your discord account
+        """
+        await ctx.respond(
+            **create_message(
+                "**Are you sure that you want to opt out of the collection and processing of your end-user-data?**\n\n"
+                "Your messages, username, discriminator, and avatar will no "
+                "longer be included in future backups and chatlogs. "
+                "Your messages will no longer be synced across channels.",
+                f=Format.WARNING
+            ),
+            components=[ActionRow(
+                Button(label="Nah, I changed my mind!", style=ButtonStyle.SUCCESS, custom_id="opt_out_cancel"),
+                Button(label="Yes!", style=ButtonStyle.DANGER, custom_id="opt_out_confirm"),
+            )],
+            ephemeral=True
+        )
+
+    @Module.component()
+    async def opt_out_confirm(self, ctx):
+        # Force user into the database
+        resp = await ctx.bot.session.get(f"https://xenon.bot/api/v1/users/{ctx.author.id}")
+        if resp.status != 200:
+            await ctx.update(**create_message(
+                "Failed to opt out, please contact a staff member.",
+                f=Format.ERROR
+            ))
+
+        await ctx.bot.db.users.update_one({"_id": ctx.author.id}, {"$set": {"privacy_opt_out": True}})
+        await ctx.update(**create_message(
+            "Okay, your have **opted out** and we will no longer collect or process your end-user-data.",
+            f=Format.SUCCESS
+        ))
+
+    @Module.component()
+    async def opt_out_cancel(self, ctx):
+        await ctx.update(**create_message(
+            "Okay, you have **not opted out**!",
+            f=Format.INFO
+        ))
+
+    @opt.sub_command(name="in")
+    async def opt_in(self, ctx):
+        """
+        Opt in to end-user-data collection for your discord account (if you have previously opted out)
+        """
+        await ctx.bot.db.users.update_one({"_id": ctx.author.id}, {"$set": {"privacy_opt_out": False}})
+        await ctx.update(**create_message(
+            "Okay, your have **opted in** and we may collect and process your end-user-data again.",
+            f=Format.SUCCESS
+        ))
